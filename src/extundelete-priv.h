e8110493d69a0da912b2eca8ff1333afadcee09e
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2018-04-12 16:01:30 +0200

    Perform initial code reformatting to make it conform to Google C++ style conventions.

--- src/extundelete-priv.h
+++ src/extundelete-priv.h
@@ -1,15 +1,18 @@
 #ifndef EXTUNDELETEPRIV_H
 #define EXTUNDELETEPRIV_H
+
 #include <climits>
 #include <cstring>
 #include <iomanip>
 #include <list>
 #include <map>
-#include <stdint.h>
+#include <cstdint>
+//#include <stdint.h>
 #include <utility>
 
 #include <sys/types.h>
 #include <ext2fs/ext2fs.h>
+
 
 // Global variables
 extern std::string outputdir;
@@ -18,7 +21,7 @@ extern std::string outputdir;
 extern uint32_t block_size_;
 
 // Information from journal
-typedef std::vector<blk64_t>  block_list_t;
+typedef std::vector<blk64_t> block_list_t;
 extern std::vector<uint32_t> tag_seq;
 extern block_list_t tag_jblk;
 extern block_list_t tag_fsblk;
@@ -29,15 +32,21 @@ extern block_list_t rvk_block;
  * in that order, for each descriptor in the journal.
  * block_pair_t is (journal block number, sequence number).
 */
-typedef std::pair<blk64_t, uint32_t>   block_pair_t;
-typedef std::pair<blk64_t, block_pair_t>  journal_map_item;
-typedef std::multimap<blk64_t, block_pair_t>  journal_map_t;
+typedef std::pair<blk64_t, uint32_t> block_pair_t;
+typedef std::pair<blk64_t, block_pair_t> journal_map_item;
+typedef std::multimap<blk64_t, block_pair_t> journal_map_t;
 extern journal_map_t journ_map;
 
-static void parse_inode_block(ext2_filsys fs, struct ext2_inode *inode, const char *buf, ext2_ino_t ino);
+static void
+parse_inode_block(ext2_filsys fs, struct ext2_inode *inode, const char *buf,
+                  ext2_ino_t ino);
+
 static errcode_t recover_inode(ext2_filsys fs, ext2_filsys jfs, ext2_ino_t ino,
-		struct ext2_inode *&inode, int ver);
-static int pair_names_with(ext2_filsys fs, ext2_filsys jfs, std::vector<ext2_ino_t>& inolist,
-		ext2_ino_t ino, std::string dirname, int del, std::vector<ext2_ino_t>& parent_inos);
+                               struct ext2_inode *&inode, int ver);
+
+static int pair_names_with(ext2_filsys fs, ext2_filsys jfs,
+                           std::vector<ext2_ino_t> &inolist,
+                           ext2_ino_t ino, std::string dirname, int del,
+                           std::vector<ext2_ino_t> &parent_inos);
 
 #endif //EXTUNDELETEPRIV_H
--- src/insertionops.cc
+++ src/insertionops.cc
@@ -1,6 +1,7 @@
 #ifndef EXT2_FLAT_INCLUDES
 #define EXT2_FLAT_INCLUDES 0
 #endif
+
 #include "config.h"
 
 #include <assert.h>
@@ -11,10 +12,10 @@
 #include <ext2fs/ext2fs.h>
 #include "extundelete.h"
 
+
 // Below are a bunch of functions to allow us to print information
 // about various types of data we encounter in this program.
-std::ostream& operator<<(std::ostream& os, const ext2_inode& inode)
-{
+std::ostream &operator<<(std::ostream &os, const ext2_inode &inode) {
   // This was largely generated with:
   //   awk 'BEGIN { decode=0 } /^struct ext2_inode / { decode=1 } /^};/ { decode=0 } { if (decode) print; }' /usr/include/ext2fs/ext2_fs.h | sed -rn 's/^[[:space:]]*(|\/\*[0-9A-F]*\*\/[[:space:]]*)__[[:alnum:]_]*[[:space:]]*(i_[[:alnum:]_]*)(;|\[[0-9]+\];)[[:space:]]*\/\*[[:space:]](.*)[[:space:]]\*\/.*/  os << "\4: " << inode.\2 << 'std::endl';/p'
 
@@ -37,22 +38,23 @@ std::ostream& operator<<(std::ostream& os, const ext2_inode& inode)
   os << "Directory ACL: " << inode.i_size_high << std::endl;
   os << "Fragment address: " << inode.i_faddr << std::endl;
   os << "Direct blocks: ";
-  for (int n = 0; n < EXT2_NDIR_BLOCKS; n++)
-  {
-    if (n!=0)
+  for (int n = 0; n < EXT2_NDIR_BLOCKS; n++) {
+    if (n != 0)
       os << ", ";
     os << inode.i_block[n];
   }
   os << std::endl;
   os << "Indirect block: " << inode.i_block[EXT2_IND_BLOCK] << std::endl;
-  os << "Double indirect block: " << inode.i_block[EXT2_DIND_BLOCK] << std::endl;
-  os << "Triple indirect block: " << inode.i_block[EXT2_TIND_BLOCK] << std::endl;
- 
+  os << "Double indirect block: " << inode.i_block[EXT2_DIND_BLOCK]
+     << std::endl;
+  os << "Triple indirect block: " << inode.i_block[EXT2_TIND_BLOCK]
+     << std::endl;
+
   return os;
 }
 
-std::ostream& operator<<(std::ostream& os, const ext2_super_block* const s_block)
-{
+std::ostream &
+operator<<(std::ostream &os, const ext2_super_block *const s_block) {
   // This was largely generated with:
   //  awk 'BEGIN { decode=0 } /^struct ext2_super_block/ { decode=1 } /^};/ { decode=0 } { if (decode) print; }' /usr/include/ext2fs/ext2_fs.h | sed -rn 's/^[[:space:]]*(|\/\*[0-9A-F]*\*\/[[:space:]]*)__[[:alnum:]_]*[[:space:]]*(s_[[:alnum:]_]*)(;|\[[0-9]+\];)[[:space:]]*\/\*[[:space:]](.*)[[:space:]]\*\/.*/  os << "\4: " << s_block->\2 << 'std::endl';/p'
   os << "Inodes count: " << s_block->s_inodes_count << std::endl;
@@ -64,7 +66,8 @@ std::ostream& operator<<(std::ostream& os, const ext2_super_block* const s_block
   os << "Block size: " << EXT2_BLOCK_SIZE(s_block) << std::endl;
   os << "Fragment size: " << EXT2_FRAG_SIZE(s_block) << std::endl;
   os << "# Blocks per group: " << s_block->s_blocks_per_group << std::endl;
-  os << "# Fragments per group: " << EXT2_FRAGS_PER_BLOCK(s_block) << std::endl;
+  os << "# Fragments per group: " << EXT2_FRAGS_PER_BLOCK(s_block)
+     << std::endl;
   os << "# Inodes per group: " << s_block->s_inodes_per_group << std::endl;
   os << "Mount time: " << s_block->s_mtime << std::endl;
   os << "Write time: " << s_block->s_wtime << std::endl;
@@ -78,39 +81,52 @@ std::ostream& operator<<(std::ostream& os, const ext2_super_block* const s_block
   os << "max. time between checks: " << s_block->s_checkinterval << std::endl;
   os << "OS: " << s_block->s_creator_os << std::endl;
   os << "Revision level: " << s_block->s_rev_level << std::endl;
-  os << "Default uid for reserved blocks: " << s_block->s_def_resuid << std::endl;
-  os << "Default gid for reserved blocks: " << s_block->s_def_resgid << std::endl;
+  os << "Default uid for reserved blocks: " << s_block->s_def_resuid
+     << std::endl;
+  os << "Default gid for reserved blocks: " << s_block->s_def_resgid
+     << std::endl;
   os << "First non-reserved inode: " << s_block->s_first_ino << std::endl;
   os << "size of inode structure: " << s_block->s_inode_size << std::endl;
-  os << "block group # of this superblock: " << s_block->s_block_group_nr << std::endl;
+  os << "block group # of this superblock: " << s_block->s_block_group_nr
+     << std::endl;
   os << "compatible feature set: " << s_block->s_feature_compat << std::endl;
-  os << "incompatible feature set: " << s_block->s_feature_incompat << std::endl;
-  os << "readonly-compatible feature set: " << s_block->s_feature_ro_compat << std::endl;
+  os << "incompatible feature set: " << s_block->s_feature_incompat
+     << std::endl;
+  os << "readonly-compatible feature set: " << s_block->s_feature_ro_compat
+     << std::endl;
   os << "128-bit uuid for volume: ";
-    for(int n = 0; n < 16; n++)
-      os << tohex((int) s_block->s_uuid[n], 2);
+  for (int n = 0; n < 16; n++)
+    os << tohex((int) s_block->s_uuid[n], 2);
   os << std::endl;
 
   os << "For compression: " << s_block->s_algorithm_usage_bitmap << std::endl;
-  os << "Nr to preallocate for dirs: " << (int) s_block->s_prealloc_dir_blocks << std::endl;
-  os << "Per group table for online growth: " << s_block->s_reserved_gdt_blocks << std::endl;
+  os << "Nr to preallocate for dirs: " << (int) s_block->s_prealloc_dir_blocks
+     << std::endl;
+  os << "Per group table for online growth: " << s_block->s_reserved_gdt_blocks
+     << std::endl;
   os << "uuid of journal superblock: ";
-    for(int n = 0; n < 16; n++)
-      os << tohex((int) s_block->s_journal_uuid[n], 2);
+  for (int n = 0; n < 16; n++)
+    os << tohex((int) s_block->s_journal_uuid[n], 2);
   os << std::endl;
 
-  os << "inode number of journal file: " << s_block->s_journal_inum << std::endl;
-  os << "device number of journal file: " << s_block->s_journal_dev << std::endl;
-  os << "start of list of inodes to delete: " << s_block->s_last_orphan << std::endl;
+  os << "inode number of journal file: " << s_block->s_journal_inum
+     << std::endl;
+  os << "device number of journal file: " << s_block->s_journal_dev
+     << std::endl;
+  os << "start of list of inodes to delete: " << s_block->s_last_orphan
+     << std::endl;
   os << "HTREE hash seed: ";
-    for(int n = 0; n < 4; n++)
-      os << tohex((unsigned int) s_block->s_hash_seed[n], 8);
+  for (int n = 0; n < 4; n++)
+    os << tohex((unsigned int) s_block->s_hash_seed[n], 8);
   os << std::endl;
 
-  os << "Default hash version to use: " << (int) s_block->s_def_hash_version << std::endl;
-  os << "Default type of journal backup: " << (int) s_block->s_jnl_backup_type << std::endl;
+  os << "Default hash version to use: " << (int) s_block->s_def_hash_version
+     << std::endl;
+  os << "Default type of journal backup: " << (int) s_block->s_jnl_backup_type
+     << std::endl;
   os << "First metablock group: " << s_block->s_first_meta_bg << std::endl;
-  os << "When the filesystem was created: " << s_block->s_mkfs_time << std::endl;
+  os << "When the filesystem was created: " << s_block->s_mkfs_time
+     << std::endl;
 //  os << "Backup of the journal inode: " << s_block->s_jnl_blocks << std::endl;
 //  os << "Padding to the end of the block: " << s_block->s_reserved << std::endl;
 
@@ -156,23 +172,23 @@ std::ostream& operator<<(std::ostream& os, const ext2_super_block* const s_block
   return os;
 }
 
-std::ostream& operator<<(std::ostream& os, const ext2_group_desc& group_desc)
-{
-  os << "block bitmap at " << group_desc.bg_block_bitmap <<
-        ", inodes bitmap at " << group_desc.bg_inode_bitmap <<
-	", inode table at " << group_desc.bg_inode_table << std::endl;
-  os << "\t   " << group_desc.bg_free_blocks_count << " free blocks, " <<
-                   group_desc.bg_free_inodes_count << " free inodes, " <<
-		   group_desc.bg_used_dirs_count << " used directory";
+std::ostream &operator<<(std::ostream &os, const ext2_group_desc &group_desc) {
+  os << "block bitmap at " << group_desc.bg_block_bitmap
+     << ", inodes bitmap at " << group_desc.bg_inode_bitmap
+     << ", inode table at " << group_desc.bg_inode_table
+     << std::endl;
+  os << "\t   "
+     << group_desc.bg_free_blocks_count << " free blocks, "
+     << group_desc.bg_free_inodes_count << " free inodes, "
+     << group_desc.bg_used_dirs_count << " used directory";
   return os;
 }
 
-std::ostream& operator<<(std::ostream& os, journal_header_t const& journal_header)
-{
+std::ostream &
+operator<<(std::ostream &os, journal_header_t const &journal_header) {
   os << "Block type: ";
   uint32_t blktype = journal_header.h_blocktype;
-  switch (blktype)
-  {
+  switch (blktype) {
     case JFS_DESCRIPTOR_BLOCK:
       os << "Descriptor block";
       break;
@@ -199,9 +215,8 @@ std::ostream& operator<<(std::ostream& os, journal_header_t const& journal_heade
 }
 
 
-
-std::ostream& operator<<(std::ostream& os, const journal_revoke_header_t journal_revoke_header)
-{
+std::ostream &operator<<(std::ostream &os,
+                         const journal_revoke_header_t journal_revoke_header) {
   os << journal_revoke_header.r_header << std::endl;
   size_t count = journal_revoke_header.r_count;
   os << "Bytes used: " << count << std::endl;
@@ -209,10 +224,11 @@ std::ostream& operator<<(std::ostream& os, const journal_revoke_header_t journal
   count -= sizeof(journal_revoke_header_t);
   assert(count % sizeof(__u32) == 0);
   count /= sizeof(__u32);
-  __u32 const* ptr = reinterpret_cast<__u32 const*>((unsigned char const*)&journal_revoke_header + sizeof(journal_revoke_header_t));
+  __u32 const *ptr = reinterpret_cast<__u32 const *>(
+    (unsigned char const *) &journal_revoke_header +
+    sizeof(journal_revoke_header_t));
   int c = 0;
-  for (uint32_t b = 0; b < static_cast<unsigned int>(count); ++b)
-  {
+  for (uint32_t b = 0; b < static_cast<unsigned int>(count); ++b) {
     std::cout << std::setfill(' ') << std::setw(8) << &ptr[b];
     ++c;
     c &= 7;
@@ -223,40 +239,50 @@ std::ostream& operator<<(std::ostream& os, const journal_revoke_header_t journal
 }
 
 
-
-
-std::ostream& operator<<(std::ostream& os, journal_superblock_t const& journal_super_block)
-{
+std::ostream &
+operator<<(std::ostream &os, journal_superblock_t const &journal_super_block) {
   os << "Journal Super Block:" << std::endl;
-  os << "Signature: 0x" << tohex(journal_super_block.s_header.h_magic, 8) << std::endl;
+  os << "Signature: 0x" << tohex(journal_super_block.s_header.h_magic, 8)
+     << std::endl;
   os << journal_super_block.s_header << std::endl;
   os << "Journal block size: " << journal_super_block.s_blocksize << std::endl;
-  os << "Number of journal blocks: " << journal_super_block.s_maxlen << std::endl;
-  os << "Journal block where the journal actually starts: " << journal_super_block.s_first << std::endl;
-  os << "Sequence number of first transaction: " << (journal_super_block.s_sequence) << std::endl;
-  os << "Journal block of first transaction: " << (journal_super_block.s_start) << std::endl;
+  os << "Number of journal blocks: " << journal_super_block.s_maxlen
+     << std::endl;
+  os << "Journal block where the journal actually starts: "
+     << journal_super_block.s_first << std::endl;
+  os << "Sequence number of first transaction: "
+     << (journal_super_block.s_sequence) << std::endl;
+  os << "Journal block of first transaction: " << (journal_super_block.s_start)
+     << std::endl;
   os << "Error number: " << (journal_super_block.s_errno) << std::endl;
   if ((journal_super_block.s_header.h_blocktype) != JFS_SUPERBLOCK_V2)
     return os;
-  os << "Compatible Features: " << (journal_super_block.s_feature_compat) << std::endl;
-  os << "Incompatible features: " << (journal_super_block.s_feature_incompat) << std::endl;
-  os << "Read only compatible features: " << (journal_super_block.s_feature_ro_compat) << std::endl;
+  os << "Compatible Features: " << (journal_super_block.s_feature_compat)
+     << std::endl;
+  os << "Incompatible features: " << (journal_super_block.s_feature_incompat)
+     << std::endl;
+  os << "Read only compatible features: "
+     << (journal_super_block.s_feature_ro_compat) << std::endl;
   os << "Journal UUID: 0x";
   for (int i = 0; i < 16; ++i)
-    os << std::hex << std::setfill('0') << std::setw(2) << (int)(journal_super_block.s_uuid[i]);
+    os << std::hex << std::setfill('0') << std::setw(2)
+       << (int) (journal_super_block.s_uuid[i]);
   os << std::dec << std::endl;
   int32_t nr_users = (journal_super_block.s_nr_users);
   os << "Number of file systems using journal: " << nr_users << std::endl;
   assert(nr_users <= 48);
-  os << "Location of superblock copy: " << (journal_super_block.s_dynsuper) << std::endl;
-  os << "Max journal blocks per transaction: " << (journal_super_block.s_max_transaction) << std::endl;
-  os << "Max file system blocks per transaction: " << (journal_super_block.s_max_trans_data) << std::endl;
+  os << "Location of superblock copy: " << (journal_super_block.s_dynsuper)
+     << std::endl;
+  os << "Max journal blocks per transaction: "
+     << (journal_super_block.s_max_transaction) << std::endl;
+  os << "Max file system blocks per transaction: "
+     << (journal_super_block.s_max_trans_data) << std::endl;
   os << "IDs of all file systems using the journal:\n";
-  for (int u = 0; u < nr_users; ++u)
-  {
+  for (int u = 0; u < nr_users; ++u) {
     os << (u + 1) << '.';
     for (int i = 0; i < 16; ++i)
-      os << std::hex << " 0x" << std::setfill('0') << std::setw(2) << (int)(journal_super_block.s_users[u * 16 + i]);
+      os << std::hex << " 0x" << std::setfill('0') << std::setw(2)
+         << (int) (journal_super_block.s_users[u * 16 + i]);
     os << std::dec << std::endl;
   }
   return os;
